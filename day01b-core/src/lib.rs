9daac8096aa8749da8cce7a924695cde89b112a5
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2021-12-02 09:25:16 +0100

    Removed left-over projects.

--- day01b-core/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::fs::File;
-use std::path::Path;
-use day01_core::gradients;
-use day01_core::sum;
--- day01b/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::path::Path;
-use day01b_core::solution_from;
-
-fn main()
-{
-	let path = Path::new("../aoc-2021/input-01");
-	let result = solution_from(&path);
-	println!("{}", &result.to_string());
-}
