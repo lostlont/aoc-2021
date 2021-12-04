db05a32eee55458289afbddf5aa9d0f3f727268b
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2021-12-04 22:56:56 +0100

    Game can be played.

--- aoc-2021-core/src/day04a.rs
+++ aoc-2021-core/src/day04a.rs
@@ -1,2 +1,50 @@
 pub use super::day04::Numbers;
 pub use super::day04::Table;
+
+pub struct Game
+{
+	numbers: Numbers,
+	tables: Vec<Table>,
+}
+
+impl Game
+{
+	pub fn from(numbers: Numbers, tables: Vec<Table>) -> Self
+	{
+		Self
+		{
+			numbers,
+			tables,
+		}
+	}
+
+	pub fn numbers(&self) -> &Numbers
+	{
+		&self.numbers
+	}
+
+	pub fn tables(&self) -> &Vec<Table>
+	{
+		&self.tables
+	}
+
+	pub fn is_finished(&self) -> bool
+	{
+		self.tables.iter().any(Table::is_bingo)
+	}
+
+	pub fn step(&mut self)
+	{
+		let number = self.numbers.pull();
+
+		for table in &mut self.tables
+		{
+			let (x, y) = table
+				.positions()
+				.filter(|p| table.number_at(p.0, p.1) == number)
+				.next()
+				.unwrap();
+			table.mark(x, y);
+		}
+	}
+}
--- aoc-2021-tests/src/day04a.rs
+++ aoc-2021-tests/src/day04a.rs
@@ -4,8 +4,93 @@ mod tests
 	//use std::path::Path;
 	use aoc_2021_core::day04a::
 	{
+		Game,
+		Numbers,
 		Table,
 	};
+
+	#[test]
+	fn game_starts_unfinished()
+	{
+		let game = create_game();
+
+		let actual = game.is_finished();
+
+		let expected = false;
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn game_step_pulls_number()
+	{
+		let mut game = create_game();
+
+		game.step();
+		let number_count = game.numbers().count();
+
+		assert_eq!(number_count, 3);
+	}
+
+	#[test]
+	fn game_step_marks_pulled_number_on_tables()
+	{
+		let mut game = create_game();
+
+		game.step();
+		let actual = game
+			.tables()
+			.iter()
+			.all(|table| table
+				.positions()
+				.all(|p|
+					{
+						let should_be_marked = table.number_at(p.0, p.1) == 3;
+						let is_marked = table.is_marked_at(p.0, p.1);
+						is_marked == should_be_marked
+					}));
+
+		assert_eq!(actual, true);
+	}
+
+	#[test]
+	fn game_step_leaves_game_unfinished_if_no_table_has_bingo()
+	{
+		let mut game = create_game();
+
+		game.step();
+		let actual = game.is_finished();
+
+		assert_eq!(actual, false);
+	}
+
+	#[test]
+	fn game_step_set_game_finished_if_a_table_has_bingo()
+	{
+		let mut game = create_game();
+
+		game.step();
+		game.step();
+		let actual = game.is_finished();
+
+		assert_eq!(actual, true);
+	}
+
+	fn create_game() -> Game
+	{
+		let numbers = Numbers::from(vec![3, 1, 2, 4]);
+		let tables = vec![
+			Table::from(vec![
+				vec![1, 2],
+				vec![4, 3],
+			]),
+			Table::from(vec![
+				vec![3, 4],
+				vec![1, 2],
+			]),
+		];
+		Game::from(numbers, tables)
+
+	}
 
 	fn create_example_table1() -> Table
 	{
