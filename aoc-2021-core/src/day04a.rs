4b484f2b7d832d859853b269adad22cc70368149
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2021-12-04 11:00:54 +0100

    Added Table struct.

--- aoc-2021-core/src/day04a.rs
+++ aoc-2021-core/src/day04a.rs
@@ -19,3 +19,103 @@ impl Numbers
 		self.0.len()
 	}
 }
+
+pub struct Table
+{
+	data: Vec<i32>,
+	marks: Vec<bool>,
+	width: i32,
+	height: i32,
+}
+
+impl Table
+{
+	pub fn from(data: Vec<Vec<i32>>) -> Self
+	{
+		Self
+		{
+			data: data.iter().cloned().flatten().collect(),
+			marks: data.iter().flatten().map(|_| false).collect(),
+			width: data[0].len() as i32,
+			height: data.len() as i32,
+		}
+	}
+
+	pub fn width(&self) -> i32
+	{
+		self.width
+	}
+
+	pub fn height(&self) -> i32
+	{
+		self.height
+	}
+
+	pub fn positions(&self) -> TablePositionIterator
+	{
+		TablePositionIterator::new(self)
+	}
+
+	pub fn is_marked_at(&self, x: i32, y: i32) -> bool
+	{
+		let index = self.to_index(x, y);
+		self.marks[index]
+	}
+
+	pub fn mark(&mut self, x: i32, y: i32)
+	{
+		let index = self.to_index(x, y);
+		self.marks[index] = true;
+	}
+
+	fn to_index(&self, x: i32, y: i32) -> usize
+	{
+		(self.width * y + x) as usize
+	}
+}
+
+pub struct TablePositionIterator
+{
+	width: i32,
+	height: i32,
+	x: i32,
+	y: i32,
+}
+
+impl TablePositionIterator
+{
+	pub fn new(table: &Table) -> Self
+	{
+		Self
+		{
+			width: table.width(),
+			height: table.height(),
+			x: 0,
+			y: 0,
+		}
+	}
+}
+
+impl Iterator for TablePositionIterator
+{
+	type Item = (i32, i32);
+
+	fn next(&mut self) -> Option<Self::Item>
+	{
+		let move_down = self.x / self.width;
+		self.x %= self.width;
+		self.y += move_down;
+
+		let result = if self.y < self.height
+		{
+			Some((self.x, self.y))
+		}
+		else
+		{
+			None
+		};
+
+		self.x += 1;
+		result
+	}
+}
--- aoc-2021-tests/src/day04a.rs
+++ aoc-2021-tests/src/day04a.rs
@@ -5,6 +5,7 @@ mod tests
 	use aoc_2021_core::day04a::
 	{
 		Numbers,
+		Table,
 	};
 
 	#[test]
@@ -40,5 +41,68 @@ mod tests
 
 		let expected = 5;
 		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn table_positions_iterates_all()
+	{
+		let table = Table::from(vec![
+			vec![1, 2],
+			vec![3, 4],
+		]);
+
+		let actual = table.positions().collect::<Vec<_>>();
+
+		let expected = vec![(0, 0), (1, 0), (0, 1), (1, 1)];
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn table_starts_unmarked()
+	{
+		let table = Table::from(vec![
+			vec![1, 2],
+			vec![3, 4],
+		]);
+
+		let actual = table
+			.positions()
+			.all(|p| table.is_marked_at(p.0, p.1) == false);
+
+		let expected = true;
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn table_mark_marks_position()
+	{
+		let mut table = Table::from(vec![
+			vec![1, 2],
+			vec![3, 4],
+		]);
+
+		table.mark(1, 1);
+
+		let actual = table
+			.positions()
+			.all(|p|
+				{
+					let should_be_marked = (p.0 == 1) && (p.1 == 1);
+					table.is_marked_at(p.0, p.1) == should_be_marked
+				});
+
+		let expected = true;
+		assert_eq!(actual, expected);
+	}
+
+	fn create_example_table1() -> Table
+	{
+		Table::from(vec![
+			vec![22, 13, 17, 11, 0],
+			vec![8, 2, 23, 4, 24],
+			vec![21, 9, 14, 16, 7],
+			vec![6, 10, 3, 18, 5],
+			vec![1, 12, 20, 15, 19],
+		])
 	}
 }
