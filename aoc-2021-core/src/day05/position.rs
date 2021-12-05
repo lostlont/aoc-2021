e9dfe578250c9de70d8dbe064c22369f926f0661
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2021-12-05 23:05:48 +0100

    Positions can be parsed.

--- aoc-2021-core/src/day05/position.rs
+++ aoc-2021-core/src/day05/position.rs
@@ -1,3 +1,7 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+use thiserror::Error;
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct Position
 {
@@ -24,5 +28,35 @@ impl Position
 	pub fn y(&self) -> usize
 	{
 		self.y
+	}
+}
+
+#[derive(Debug, Error, PartialEq)]
+pub enum ParsePositionError
+{
+	#[error("String does not contain two values: {0:?}!")]
+	SplitError(String),
+
+	#[error("Parse error: {0}!")]
+	ValueError(#[from] ParseIntError),
+}
+
+impl FromStr for Position
+{
+	type Err = ParsePositionError;
+
+	fn from_str(string: &str) -> Result<Self, Self::Err>
+	{
+		let mut split = string.split(',');
+		let x = split
+			.next()
+			.ok_or(ParsePositionError::SplitError(string.to_string()))?
+			.parse::<usize>()?;
+		let y = split
+			.next()
+			.ok_or(ParsePositionError::SplitError(string.to_string()))?
+			.parse::<usize>()?;
+
+		Ok(Position::from(x, y))
 	}
 }
--- aoc-2021-tests/src/day05/position.rs
+++ aoc-2021-tests/src/day05/position.rs
@@ -7,21 +7,36 @@ mod test
 	fn position_x()
 	{
 		let at = Position::from(2, 3);
-
 		let actual = at.x();
-
-		let expected = 2;
-		assert_eq!(actual, expected);
+		assert_eq!(actual, 2);
 	}
 
 	#[test]
 	fn position_y()
 	{
 		let at = Position::from(2, 3);
-
 		let actual = at.y();
+		assert_eq!(actual, 3);
+	}
 
-		let expected = 3;
-		assert_eq!(actual, expected);
+	#[test]
+	fn from_str_returns_error()
+	{
+		let actual = "".parse::<Position>();
+		assert!(actual.is_err());
+	}
+
+	#[test]
+	fn from_str_parses_zero()
+	{
+		let actual = "0,0".parse::<Position>();
+		assert_eq!(actual, Ok(Position::from(0, 0)));
+	}
+
+	#[test]
+	fn from_str_parses_values()
+	{
+		let actual = "1,2".parse::<Position>();
+		assert_eq!(actual, Ok(Position::from(1, 2)));
 	}
 }
