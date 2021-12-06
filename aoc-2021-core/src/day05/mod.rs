704adb9c48ea9e58144d6ff016b504ec5e95e56b
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2021-12-06 10:41:15 +0100

    Iterating over lines.

--- aoc-2021-core/src/day05/mod.rs
+++ aoc-2021-core/src/day05/mod.rs
@@ -1,5 +1,64 @@
+use thiserror::Error;
+
 mod position;
 mod sparse_table;
 
 pub use position::Position;
 pub use sparse_table::SparseTable;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum LineError
+{
+	#[error("Diagonal lines can not be handled: {start:?} -> {end:?}!")]
+	DiagonalLine
+	{
+		start: Position,
+		end: Position,
+	},
+}
+
+pub fn line(start: Position, end: Position) -> Result<Vec<Position>, LineError>
+{
+	if start == end
+	{
+		Ok(vec![start])
+	}
+	else if start.x() == end.x()
+	{
+		let result = if start.y() < end.y()
+		{
+			(start.y()..=end.y())
+				.map(|y| Position::from(start.x(), y))
+				.collect()
+		}
+		else
+		{
+			(end.y()..=start.y())
+				.rev()
+				.map(|y| Position::from(start.x(), y))
+				.collect()
+		};
+		Ok(result)
+	}
+	else if start.y() == end.y()
+	{
+		let result = if start.x() < end.x()
+		{
+			(start.x()..=end.x())
+				.map(|x| Position::from(x, start.y()))
+				.collect()
+		}
+		else
+		{
+			(end.x()..=start.x())
+				.rev()
+				.map(|x| Position::from(x, start.y()))
+				.collect()
+		};
+		Ok(result)
+	}
+	else
+	{
+		Err(LineError::DiagonalLine{ start, end })
+	}
+}
--- aoc-2021-tests/src/day05/mod.rs
+++ aoc-2021-tests/src/day05/mod.rs
@@ -1,2 +1,100 @@
 mod position;
 mod sparse_table;
+
+#[cfg(test)]
+mod test
+{
+	use aoc_2021_core::day05::
+	{
+		line,
+		LineError,
+		Position,
+	};
+
+	#[test]
+	fn line_has_single_position_for_same_start_end()
+	{
+		let start = Position::from(2, 3);
+		let end = Position::from(2, 3);
+
+		let actual = line(start, end);
+
+		let expected = Ok(vec![start]);
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn line_has_two_positions_for_end_right_to_start()
+	{
+		let start = Position::from(5, 5);
+		let end = Position::from(6, 5);
+
+		let actual = line(start, end);
+
+		let expected = Ok(vec![start, end]);
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn line_has_two_positions_for_end_left_to_start()
+	{
+		let start = Position::from(5, 5);
+		let end = Position::from(4, 5);
+
+		let actual = line(start, end);
+
+		let expected = Ok(vec![start, end]);
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn line_has_two_positions_for_end_below_start()
+	{
+		let start = Position::from(5, 5);
+		let end = Position::from(5, 6);
+
+		let actual = line(start, end);
+
+		let expected = Ok(vec![start, end]);
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn line_has_two_positions_for_end_above_start()
+	{
+		let start = Position::from(5, 5);
+		let end = Position::from(5, 4);
+
+		let actual = line(start, end);
+
+		let expected = Ok(vec![start, end]);
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn line_has_multiple_positions_for_long_line()
+	{
+		let start = Position::from(5, 5);
+		let end = Position::from(10, 5);
+
+		let actual = line(start, end);
+
+		let expected = (5..=10)
+			.map(|x| Position::from(x, 5))
+			.collect::<Vec<_>>();
+		let expected = Ok(expected);
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn line_returns_error_for_diagonal_line()
+	{
+		let start = Position::from(5, 5);
+		let end = Position::from(6, 6);
+
+		let actual = line(start, end);
+
+		let expected = Err(LineError::DiagonalLine{ start, end });
+		assert_eq!(actual, expected);
+	}
+}
