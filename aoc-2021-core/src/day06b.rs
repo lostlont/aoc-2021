ccdf28472ab7582e975366e26af1e33bf77ceeb2
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2021-12-19 22:23:05 +0100

    Solution 06/b.

---
+++ aoc-2021-core/src/day06b.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use super::day06::parse;
+use super::day06::ParseError;
+use super::day06::Simulation;
+use super::day06::solution;
+
+pub fn solution_from(path: &Path) -> Result<usize, ParseError>
+{
+	let file = File::open(&path)?;
+	let reader = BufReader::new(file);
+	let input = parse(reader)?;
+	let simulation = Simulation::new(input, 8, 6, 256);
+	Ok(solution(simulation))
+}
--- aoc-2021-tests/src/day06b.rs
+++ aoc-2021-tests/src/day06b.rs
@@ -1,10 +1,15 @@
 #[cfg(test)]
 mod tests
 {
+	use std::path::Path;
 	use aoc_2021_core::day06::
 	{
 		Simulation,
 		solution,
+	};
+	use aoc_2021_core::day06b::
+	{
+		solution_from,
 	};
 	use super::super::day06::tests::create_example_input;
 
@@ -21,5 +26,13 @@ mod tests
 		let actual = solution(simulation);
 
 		assert_eq!(actual, 26984457539);
+	}
+
+	#[test]
+	fn solution_is_correct()
+	{
+		let path = Path::new("../aoc-2021/input-06");
+		let actual = solution_from(&path).unwrap();
+		assert_eq!(actual, 1595779846729);
 	}
 }
